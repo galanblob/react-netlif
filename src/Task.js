ba3d9bf5cab1bbe6f2277cd9f126b7fc7bcffc7f
galangahara <galan.gahara@ui.ac.id>
2023-06-19 17:15:01 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Galan Gahara Triguna Toding" // TODO: replace this
+const studentId = "FE4510207" // TODO: replace this
+
+export { url, studentName, studentId }
